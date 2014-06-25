45856cef988690414870fea1e0f209de4bbdded7
Mike <m@thecoded.com>
2014-06-25 03:49:10 -0400

    undid 'fixes'

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var PORT =  80;
+var PORT = process.env.OPENSHIFT_INTERNAL_PORT || process.env.OPENSHIFT_NODEJS_PORT  || 8080;
 var IPADDRESS = process.env.OPENSHIFT_INTERNAL_IP || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 var express = require('express');
